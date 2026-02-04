1a5c47d259f7ef024b47d1aa54b6134d203c83bc
FrankyOwen <148160434+FrankyOwen@users.noreply.github.com>
2026-02-04 15:28:32 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,11 +1,9 @@
 // keep_alive.js
-import http from 'http';
+const http = require('http');
 
-export default function keep_alive() {
-  http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("I'm alive");
-  }).listen(8080, () => {
-    console.log('Keep-alive server running on port 8080');
-  });
-}
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("I'm alive");
+}).listen(8080, () => {
+  console.log('Keep-alive server running on port 8080');
+});
